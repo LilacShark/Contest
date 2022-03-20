2ebcd1f12653bc4cacd18a6581d62e32621c0704
TrickyObject <krasusworks@gmail.com>
2022-03-20 19:55:08 +0700

    Up to page 116 -except SpEL -except p-namespace

---
+++ src/main/java/sia/example/contest/OneManBand.java
@@ -0,0 +1,46 @@
+package sia.example.contest;
+
+import java.util.Collection;
+import java.util.Map;
+import java.util.Properties;
+
+public class OneManBand implements Performer{
+
+    private Collection<Instrument> instruments; //для внедрения коллекций
+    private Map<String, Instrument> instrumentMap; // для внедрения отображений мап
+    private Properties instrumentsProp; // для внедрения свойств
+
+    public OneManBand() {
+    }
+
+    @Override
+    public void perform() throws PerformanceException {
+        // обработка коллекции
+//        for (Instrument instrument: instruments) {
+//            instrument.play();
+//        }
+
+        // обработка отображений
+//        for (String key: instrumentMap.keySet()) {
+//            System.out.println(key + " : ");
+//            Instrument instrument = instrumentMap.get(key);
+//            instrument.play();
+
+        // обработка свойств
+
+        }
+    }
+
+    public void setInstruments(Collection<Instrument> instruments) {
+        this.instruments = instruments;
+    }
+
+    public void setInstrumentMap(Map<String, Instrument> instrumentMap) {
+        this.instrumentMap = instrumentMap;
+    }
+
+    public void setInstruments(Properties instruments) {
+        this.instrumentsProp = instruments;
+    }
+
+}
